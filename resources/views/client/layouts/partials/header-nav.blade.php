e592de819a17293558cb66e478b9405403b7f643
dogiap04 <dogiap2k4@gmail.com>
2024-07-22 11:59:13 +0700

    ASM-PHP3

---
+++ resources/views/client/layouts/partials/header-nav.blade.php
@@ -0,0 +1,34 @@
+<nav class="site-navigation text-right text-md-center" role="navigation">
+    <div class="container">
+        <ul class="site-menu js-clone-nav d-none d-md-block">
+            <li class="has-children active">
+                <a href="/index">Home</a>
+                <ul class="dropdown">
+                    <li><a href="#">Menu One</a></li>
+                    <li><a href="#">Menu Two</a></li>
+                    <li><a href="#">Menu Three</a></li>
+                    <li class="has-children">
+                        <a href="#">Sub Menu</a>
+                        <ul class="dropdown">
+                            <li><a href="#">Menu One</a></li>
+                            <li><a href="#">Menu Two</a></li>
+                            <li><a href="#">Menu Three</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-children">
+                <a href="/about">Chúng tôi</a>
+                <ul class="dropdown">
+                    <li><a href="#">Menu One</a></li>
+                    <li><a href="#">Menu Two</a></li>
+                    <li><a href="#">Menu Three</a></li>
+                </ul>
+            </li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/post">Tin tức</a></li>
+            <li><a href="#">Hàng mới</a></li>
+            <li><a href="contact.html">Liên hệ</a></li>
+        </ul>
+    </div>
+</nav>
